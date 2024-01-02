d46c8ab0817b3891e7e984ebf2a0db763b60f420
kevinnkimm <43616290+kevinnkimm@users.noreply.github.com>
2024-01-02 02:41:00 -0500

    added a mousedrag feature to change grid colors

--- script.js
+++ script.js
@@ -9,13 +9,38 @@ function createGrid() {
         for (let j = 0; j < columns; j++) {
             const gridItem = document.createElement('div');
             gridItem.className = 'grid-item';
+            // leaves a trail when dragging mouse over the grid
+            
+            gridItem.addEventListener('mousedown', () => {
+                isDragging = true;
+                gridItem.style.backgroundColor = 'white';
+            });
 
+            gridItem.addEventListener('mousemove', () => {
+                if (isDragging) {
+                    gridItem.style.backgroundColor = 'white';
+                }
+            });
+            gridItem.addEventListener('mouseup', () => {
+                isDragging = false;
+            });
+            
             gridContainer.appendChild(gridItem);
         }
     }
 }
 
 createGrid();
+
+// user pop up button
+
+const userBtn = document.createElement('button');
+
+
+
+
+
+
 
 
 
